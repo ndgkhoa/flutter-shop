8105686d19531714c21d0f36aecfbe8fbcfa0e3b
khoa <user.iamjasper@gmail.com>
2024-05-31 06:18:00 +0700

    add ProductCard

--- lib/views/ui/homepage.dart
+++ lib/views/ui/homepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/views/shared/product_card.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_application_1/views/shared/appstyle.dart';
@@ -18,157 +20,169 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color(0xFFE2E2E2),
         body: SizedBox(
-      height: MediaQuery.of(context).size.height,
-      child: Stack(
-        children: [
-          Container(
-            padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
-            height: MediaQuery.of(context).size.height * 0.4,
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/top_image.png"),
-                    fit: BoxFit.fill)),
-            child: Container(
-              padding: const EdgeInsets.only(left: 8, bottom: 15),
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Athletics Shoes",
-                    style:
-                        appstyleWithHt(42, Colors.white, FontWeight.bold, 1.5),
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
+                height: MediaQuery.of(context).size.height * 0.4,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/top_image.png"),
+                        fit: BoxFit.fill)),
+                child: Container(
+                  padding: const EdgeInsets.only(left: 8, bottom: 15),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Athletics Shoes",
+                        style: appstyleWithHt(
+                            42, Colors.white, FontWeight.bold, 1.5),
+                      ),
+                      Text(
+                        "Collection",
+                        style: appstyleWithHt(
+                            42, Colors.white, FontWeight.bold, 1.2),
+                      ),
+                      TabBar(
+                          padding: EdgeInsets.zero,
+                          indicatorSize: TabBarIndicatorSize.label,
+                          indicatorColor: Colors.transparent,
+                          controller: _tabController,
+                          isScrollable: true,
+                          labelColor: Colors.white,
+                          labelStyle:
+                              appstyle(24, Colors.white, FontWeight.bold),
+                          unselectedLabelColor: Colors.grey.withOpacity(0.3),
+                          tabs: const [
+                            Tab(
+                              text: "Men shoes",
+                            ),
+                            Tab(
+                              text: "Women shoes",
+                            ),
+                            Tab(
+                              text: "Kids shoes",
+                            ),
+                          ]),
+                    ],
                   ),
-                  Text(
-                    "Collection",
-                    style:
-                        appstyleWithHt(42, Colors.white, FontWeight.bold, 1.2),
-                  ),
-                  TabBar(
-                      indicatorSize: TabBarIndicatorSize.label,
-                      indicatorColor: Colors.transparent,
-                      controller: _tabController,
-                      isScrollable: true,
-                      labelColor: Colors.white,
-                      labelStyle: appstyle(24, Colors.white, FontWeight.bold),
-                      unselectedLabelColor: Colors.grey.withOpacity(0.3),
-                      tabs: const [
-                        Tab(
-                          text: "Men shoes",
-                        ),
-                        Tab(
-                          text: "Women shoes",
-                        ),
-                        Tab(
-                          text: "Kids shoes",
-                        ),
-                      ]),
-                ],
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.265),
-            child: Container(
-              padding: const EdgeInsets.only(left: 12),
-              child: TabBarView(controller: _tabController, children: [
-                Column(
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.405,
-                      child: ListView.builder(
-                          itemCount: 6,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                color: Colors.grey,
-                                height: MediaQuery.of(context).size.height,
-                                width: MediaQuery.of(context).size.width * 0.6,
-                              ),
-                            );
-                          }),
-                    ),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.265),
+                child: Container(
+                  padding: const EdgeInsets.only(left: 12),
+                  child: TabBarView(controller: _tabController, children: [
                     Column(
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(12, 20, 12, 20),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "Lastest Shoes",
-                                style:
-                                    appstyle(24, Colors.black, FontWeight.bold),
-                              ),
-                              Row(
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.405,
+                          child: ListView.builder(
+                              itemCount: 6,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, index) {
+                                return ProductCart(
+                                    price: "\$20.35",
+                                    category: "Men Shoes",
+                                    id: "1",
+                                    name: "Nike Supper Vippro",
+                                    image:
+                                        "https://d326fntlu7tb1e.cloudfront.net/uploads/58282ea3-b815-4d26-9f4f-382aa62f67cf-HP5404_a1.webp");
+                              }),
+                        ),
+                        Column(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(12, 20, 12, 20),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
                                   Text(
-                                    "Show all",
+                                    "Lastest Shoes",
                                     style: appstyle(
-                                        22, Colors.black, FontWeight.w500),
+                                        24, Colors.black, FontWeight.bold),
                                   ),
-                                  Icon(
-                                    AntDesign.caretright,
-                                    size: 20,
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Show all",
+                                        style: appstyle(
+                                            22, Colors.black, FontWeight.w500),
+                                      ),
+                                      const Icon(
+                                        AntDesign.caretright,
+                                        size: 20,
+                                      )
+                                    ],
                                   )
                                 ],
-                              )
-                            ],
-                          ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.13,
+                          child: ListView.builder(
+                              itemCount: 6,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(16)),
+                                        boxShadow: [
+                                          BoxShadow(
+                                              color: Colors.white,
+                                              spreadRadius: 1,
+                                              blurRadius: 0.8,
+                                              offset: Offset(0, 1))
+                                        ]),
+                                    height: MediaQuery.of(context).size.height *
+                                        0.12,
+                                    width: MediaQuery.of(context).size.width *
+                                        0.28,
+                                    child: CachedNetworkImage(
+                                      imageUrl:
+                                          "https://d326fntlu7tb1e.cloudfront.net/uploads/58282ea3-b815-4d26-9f4f-382aa62f67cf-HP5404_a1.webp",
+                                    ),
+                                  ),
+                                );
+                              }),
                         )
                       ],
                     ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.13,
-                      child: ListView.builder(
-                          itemCount: 6,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    color: Colors.grey,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(16))),
-                                height:
-                                    MediaQuery.of(context).size.height * 0.12,
-                                width: MediaQuery.of(context).size.width * 0.28,
-                                child: CachedNetworkImage(
-                                  imageUrl:
-                                      "https://d326fntlu7tb1e.cloudfront.net/uploads/58282ea3-b815-4d26-9f4f-382aa62f67cf-HP5404_a1.webp",
-                                ),
-                              ),
-                            );
-                          }),
-                    )
-                  ],
+                    Column(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.405,
+                          color: Colors.green,
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.405,
+                          color: Colors.amber,
+                        )
+                      ],
+                    ),
+                  ]),
                 ),
-                Column(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.405,
-                      color: Colors.green,
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.405,
-                      color: Colors.amber,
-                    )
-                  ],
-                ),
-              ]),
-            ),
-          )
-        ],
-      ),
-    ));
+              )
+            ],
+          ),
+        ));
   }
 }
